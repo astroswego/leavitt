1d7c7feee6e284df311799542d40ffe609324ab3
Dan Wysocki <dwysocki@oswego.edu>
2015-01-31 22:13:10 -0500

    Added in option to fit PL relation without individual distances

--- src/leavitt/leavitt.py
+++ src/leavitt/leavitt.py
@@ -1,15 +1,21 @@
 from argparse import ArgumentError, ArgumentParser, FileType, SUPPRESS
-from sys import exit, stdin, stdout, stderr
+from sys import argv, exit, stdin, stdout, stderr
 from os import path
 import numpy
 import pandas
 from pandas import DataFrame
 from pandas.io.parsers import read_table
-from itertools import chain, count, product
-from leavitt.regression import fit_with_sigma_clip
+from itertools import chain, product
+from leavitt.regression import leavitt_law
 from leavitt.error import monte_carlo
-from leavitt.utils import char, zscore
+from leavitt.utils import (any_nan, char, identity, modulii2pc, modulii2kpc,
+                           zscore)
 
+unit_conversion_choices = {
+    "modulii" : identity,
+    "pc"      : modulii2pc,
+    "kpc"     : modulii2kpc
+}
 error_choices = {
     "None" : None,
     "Monte_Carlo" : monte_carlo
@@ -28,10 +34,11 @@ def add_coeff_rows(frame, independent_vars, dependent_vars, add_const,
         frame.loc[coeff_name] = numpy.nan
     
 
-def get_args():
+def get_args(argv=argv[1:]):
     parser = ArgumentParser(prog="leavitt")
 
     general_group    = parser.add_argument_group("General")
+    parallel_group   = parser.add_argument_group("Parallel")
     format_group     = parser.add_argument_group("Formatting")
     regression_group = parser.add_argument_group("Regression")
     outlier_group    = parser.add_argument_group("Outlier Detection")
@@ -52,6 +59,12 @@ def get_args():
              "modulii "
              "(default = 0.0)")
 
+    ## Parallel Options ##
+    parallel_group.add_argument("--error-processes", type=int,
+        default=1,
+        help="Number of processes to use in error determination "
+             "(default = 1)")
+
     ## Formatting Options ##
     format_group.add_argument("-f", "--format", type=str,
         default="%.5f",
@@ -65,6 +78,14 @@ def get_args():
         default="\t",
         help="Separator in output tables (a single character) "
              "(default = \t)")
+    format_group.add_argument("--na-values", type=str, nargs="+",
+        default=["NA"],
+        help="String(s) used to denote N/A values in input table "
+             "(default = NA)")
+    format_group.add_argument("--output-na-value", type=str,
+        default="NA",
+        help="String used to denote N/A values in output table "
+             "(default = NA)")
     format_group.add_argument("--distance-label", type=str, metavar="LABEL",
         default="dist",
         help="Label for all distance columns. "
@@ -80,8 +101,9 @@ def get_args():
         metavar="VAR", required=True,
         help="List of independent variables to regress on")
     regression_group.add_argument("--add-const", action="store_true",
-        default=False,
         help="Add a constant term to each equation")
+    regression_group.add_argument("--fit-modulus", action="store_true",
+        help="Fit individual distance modulii")
     regression_group.add_argument("--rcond", type=float,
         default=1e-3,
         help="Singular values are set to zero if they are smaller than "
@@ -127,15 +149,20 @@ def get_args():
              "if applicable "
              "(default = 1000)")
 
-    args = parser.parse_args()
+    args = parser.parse_args(argv)
 
     args.error_method = error_choices[args.error_method]
     if args.error_method is not None \
        and args.error_prefix == args.error_suffix == "":
             raise ArgumentError("An error prefix or suffix must be provided "
                                 "for error to be computed")
+    args.distance_error_label = args.error_prefix   \
+                              + args.distance_label \
+                              + args.error_suffix
 
     args.sigma_method = sigma_choices[args.sigma_method]
+
+    args.units = unit_conversion_choices[args.units]
 
     return args
 
@@ -143,7 +170,8 @@ def main(args=None):
     if args is None:
         args = get_args()
 
-    data = read_table(args.input, index_col=0, sep=args.sep,
+    data = read_table(args.input, index_col=0,
+                      sep=args.sep, na_values=args.na_values,
                       engine="python")
     add_coeff_rows(data,
                    args.independent_vars, args.dependent_vars, args.add_const)
@@ -161,35 +189,55 @@ def main(args=None):
 
     if args.error_method is not None:
         try:
-            independent_vars_error = data[
+            dependent_vars_error = data[
                 [args.error_prefix + var + args.error_suffix
-                 for var in args.independent_vars]
+                 for var in args.dependent_vars]
             ].iloc[:-n_coeff].values
+
         except KeyError as e:
             key = e.args[0]
             print("Missing entry in input table for error: {}".format(key),
                   file=stderr)
             return 1
 
+        nan_rows = any_nan(dependent_vars,
+                           independent_vars,
+                           dependent_vars_error,
+                           axis=1)
+        non_nan_rows = ~nan_rows
+        dependent_vars       =   dependent_vars[    non_nan_rows]
+        independent_vars     = independent_vars[    non_nan_rows]
+        dependent_vars_error = dependent_vars_error[non_nan_rows]
+        mask = numpy.concatenate((non_nan_rows,
+                                  numpy.ones(n_coeff, dtype=bool)))
+
         fit, fit_err = args.error_method(dependent_vars,
                                          independent_vars,
-                                         independent_vars_error,
-                                         args.add_const,
+                                         dependent_vars_error,
+                                         args.add_const, args.fit_modulus,
                                          args.sigma_method, args.sigma,
-                                         args.mean_modulus, args.units)
-        data[args.distance_label] = fit
-        data[args.error_prefix
-           + args.distance_label
-           + args.error_suffix] = fit_err
+                                         args.mean_modulus, args.units,
+                                         args.rcond, args.sigma_max_iter,
+                                         args.error_iterations,
+                                         args.error_processes)
+        data[args.distance_label      ] = fit
+        data[args.distance_error_label] = fit_err
     else:
-        fit = fit_with_sigma_clip(dependent_vars, independent_vars,
-                                  args.add_const,
-                                  args.sigma_method, args.sigma,
-                                  args.mean_modulus, args.units,
-                                  args.rcond, args.sigma_max_iter)
-        data[args.distance_label] = fit
+        nan_rows = any_nan(dependent_vars, independent_vars, axis=1)
+        non_nan_rows = ~nan_rows
+        dependent_vars   =   dependent_vars[non_nan_rows]
+        independent_vars = independent_vars[non_nan_rows]
+        mask = numpy.concatenate((non_nan_rows,
+                                  numpy.ones(n_coeff, dtype=bool)))
+        fit = leavitt_law(dependent_vars, independent_vars,
+                          args.add_const, args.fit_modulus,
+                          args.sigma_method, args.sigma,
+                          args.mean_modulus, args.units,
+                          args.rcond, args.sigma_max_iter)
+        data.loc[mask, args.distance_label] = fit
 
-    data.to_csv(stdout, sep=args.output_sep, na_rep="NaN")
+    data.to_csv(stdout, sep=args.output_sep, float_format=args.format,
+                na_rep=args.output_na_value)
 
     return 0
 
--- src/leavitt/regression.py
+++ src/leavitt/regression.py
@@ -1,13 +1,32 @@
 import numpy
-from leavitt.utils import convert_units, zscore
+from leavitt.utils import identity, zscore
 
 __all__ = [
-    "distance_formula",
-    "fit_with_sigma_clip"
+    "design_matrix",
+    "leavitt_law"
 ]
 
+def simple_design_matrix(independent_vars, add_const, n_dependent_vars):
+    n_samples = independent_vars.shape[0]
 
-def distance_formula(dependent_vars, independent_vars, add_const=False):
+    if add_const:
+        constants = numpy.ones((n_samples, 1))
+        independent_vars = numpy.hstack((independent_vars, constants))
+
+    n_samples, n_independent_vars = independent_vars.shape
+    design_matrix = numpy.zeros((n_dependent_vars*n_samples,
+                                 n_dependent_vars*n_independent_vars),
+                                dtype=float)
+
+    for i in range(n_dependent_vars):
+        design_matrix[
+            i*n_samples : (i+1)*n_samples,
+            i*n_independent_vars : (i+1)*n_independent_vars
+        ] = independent_vars
+
+    return design_matrix
+
+def design_matrix(dependent_vars, independent_vars, add_const=False):
     if add_const:
         independent_vars = numpy.hstack((independent_vars,
                                          numpy.ones((independent_vars.shape[0],
@@ -30,19 +49,43 @@ def distance_formula(dependent_vars, independent_vars, add_const=False):
     return design_matrix, numpy.reshape(dependent_vars, -1)
 
 
-def fit_with_sigma_clip(dependent_vars, independent_vars, add_const=False,
-                        sigma_method=zscore, sigma=0.0,
-                        mean_modulus=0.0, units="modulii",
-                        rcond=1e-3, max_iter=20):
+def simple_leavitt_law(dependent_vars, independent_vars, add_const, rcond):
+    n_samples, n_vars = dependent_vars.shape
+
+    X = simple_design_matrix(independent_vars, add_const, n_vars)
+    y = numpy.reshape(dependent_vars, -1)
+
+    b, residuals, rank, s = numpy.linalg.lstsq(X, y, rcond=rcond)
+
+    n_coeff = b.size
+
+    fit = numpy.empty(n_samples+n_coeff, dtype=float)
+    fit[:-n_coeff] = numpy.nan
+    fit[-n_coeff:] = b
+
+    return fit
+
+
+
+
+def leavitt_law(dependent_vars, independent_vars, add_const=False,
+                fit_modulus=False,
+                sigma_method=zscore, sigma=0.0,
+                mean_modulus=0.0, unit_conversion=identity,
+                rcond=1e-3, max_iter=20):
     n_samples, n_vars = dependent_vars.shape
     n_coeff = (1+add_const)*n_vars
+
+    # if not fitting modulus, do simple PL fit
+    if not fit_modulus:
+        return simple_leavitt_law(dependent_vars, independent_vars,
+                                  add_const, rcond)
     # if sigma is 0 or less, do not perform any outlier detection
     if sigma <= 0:
-        X, y = distance_formula(dependent_vars, independent_vars,
-                                add_const)
+        X, y = design_matrix(dependent_vars, independent_vars, add_const)
         # solve ``X*b = y`` for ``b``
         b, residuals, rank, s = numpy.linalg.lstsq(X, y, rcond=rcond)
-        d = convert_units(b[:-n_coeff]+mean_modulus, units)
+        d = unit_conversion(b[:-n_coeff]+mean_modulus)
         return numpy.concatenate((d, b[-n_coeff:]))
 
     if max_iter < 1:
@@ -52,12 +95,12 @@ def fit_with_sigma_clip(dependent_vars, independent_vars, add_const=False,
     mask = numpy.ones(n_samples, dtype=bool)
 
     for i in range(max_iter):
-        X, y = distance_formula(dependent_vars[mask], independent_vars[mask],
-                                add_const)
+        X, y = design_matrix(dependent_vars[mask], independent_vars[mask],
+                             add_const)
         # solve ``X*b = y`` for ``b``
         b, residuals, rank, s = numpy.linalg.lstsq(X, y, rcond=rcond)
         # extract distances from ``b`` and convert units
-        dist = convert_units(b[:-n_coeff]+mean_modulus, units)
+        dist = unit_conversion(b[:-n_coeff]+mean_modulus)
         new_mask = abs(sigma_method(dist)) < sigma
         if numpy.array_equal(mask[mask], new_mask):
             # no new outliers detected
@@ -66,10 +109,9 @@ def fit_with_sigma_clip(dependent_vars, independent_vars, add_const=False,
             return _format_fit(dist, b[-n_coeff:], mask, n_samples, n_coeff)
         else:
             # new outliers detected, mask them and continue sigma clipping
-            print(mask.shape, new_mask.shape)
             mask[mask] = new_mask
             continue
-
+    # reached max iterations
     return _format_fit(dist[new_mask], b[-n_coeff:], mask, n_samples, n_coeff)
 
 
